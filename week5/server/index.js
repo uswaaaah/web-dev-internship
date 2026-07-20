56da89d92f2342f68414d50ab838d201e6ea020d
uswaaaah <uswahnadir272@gmail.com>
2026-07-20 19:03:25 +0500

    Add routing and HTTP methods practice

--- week5/server/index.js
+++ week5/server/index.js
@@ -2,8 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// Home route
 app.get("/", (req, res) => {
   res.send("Hello from my first server!");
+});
+
+// About route
+app.get("/about", (req, res) => {
+  res.send("This is the About page.");
+});
+
+// Students route
+app.get("/students", (req, res) => {
+  res.send("List of students would go here.");
+});
+
+// Route parameter
+app.get("/students/:id", (req, res) => {
+  res.send(`Student ID requested: ${req.params.id}`);
+});
+
+// Query parameter
+app.get("/search", (req, res) => {
+  res.send(`Searching for: ${req.query.name}`);
 });
 
 app.listen(PORT, () => {
