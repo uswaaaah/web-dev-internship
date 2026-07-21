d17733c6a88bc879846c4d2aaa867137ccacd01e
uswaaaah <uswahnadir272@gmail.com>
2026-07-21 18:19:52 +0500

    Add Student schema and create/insert route

--- week6/server/index.js
+++ week6/server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const Student = require("./models/Student");
 
 const app = express();
 
@@ -10,6 +11,21 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Connection error:", err));
+
+app.post("/api/students", async (req, res) => {
+  try {
+    const newStudent = new Student(req.body);
+
+    await newStudent.save();
+
+    res.status(201).json(newStudent);
+
+  } catch (err) {
+    res.status(400).json({
+      error: err.message,
+    });
+  }
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on http://localhost:${process.env.PORT}`);
